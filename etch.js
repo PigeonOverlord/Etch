908c6c63472f2150fe6ee021c78d08f880e593ed
PigeonOverlord <peterwallace2k5@gmail.com>
2022-10-13 13:37:10 +0100

    Enabled css grid for container - createDiv function takes argument and sets grid to size

--- etch.js
+++ etch.js
@@ -2,10 +2,13 @@ const container = document.querySelector("#container");
 const div = document.createElement("div");
 
 function createDivs (num){
+        container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     for (let i = 0; i < num; i++) {
         const div = document.createElement("div");
         container.appendChild(div);
-        div.classList.add('div')
+        div.classList.add('div');
+        
 
       }
 }
