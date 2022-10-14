ef32a4714eafafb4521075e0825ea6e57cf9b8a1
PigeonOverlord <peterwallace2k5@gmail.com>
2022-10-14 16:39:31 +0100

    grid scaleable, hover elements added to div, slider added  with innerHTML display

--- etch.js
+++ etch.js
@@ -1,15 +1,25 @@
 const container = document.querySelector("#container");
 const div = document.createElement("div");
+const slideValue = document.querySelector("#slideValue")
+const slider = document.querySelector("#slider")
+var numpo = 0;
+
+slider.oninput = function() {
+    slideValue.innerHTML = this.value + " x " + this.value;
+    console.log(this.value);
+    return this.value;
+  } 
+
 
 function createDivs (num){
-        container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${num}, 1fr)`; // sets grid column width
+        container.style.gridTemplateRows = `repeat(${num}, 1fr)`; // sets gride rows length
+        num = num * num; // sets grid size
     for (let i = 0; i < num; i++) {
         const div = document.createElement("div");
         container.appendChild(div);
-        div.classList.add('div');
+        div.classList.add('gridBlock');
         
 
       }
 }
-
